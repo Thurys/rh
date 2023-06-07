acbd19d7753c74aeb7e7a17492afdad2fa5b0573
Artus Krieger <artus.krieger@gmail.com>
2023-06-07 17:11:58 +0200

    Add Dash.js video player

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { VideoCardListComponent } from './video-card-list/video-card-list.component';
-import { VideoCardComponent } from './video-card/video-card.component';
+import { VideoPlayerComponent } from './video-player/video-player.component';
 
 const routes: Routes = [
-  { path: '', component: VideoCardListComponent },
-  { path: 'card', component: VideoCardComponent },
+  { path: '', component: VideoPlayerComponent },
+  { path: 'list', component: VideoCardListComponent },
 ];
 
 @NgModule({
--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component } from '@angular/core';
+
+declare var dashjs: any;
 
 @Component({
   selector: 'app-video-player',
   templateUrl: './video-player.component.html',
   styleUrls: ['./video-player.component.scss']
 })
-export class VideoPlayerComponent {
-
+export class VideoPlayerComponent implements AfterContentInit {
+  ngAfterContentInit(): void {
+    let url: string = "https://dash.akamaized.net/akamai/bbb_30fps/bbb_30fps.mpd";
+    let player = dashjs.MediaPlayer().create();
+    player.initialize(document.querySelector("#videoPlayer"), url, true);
+    player.setAutoPlay(true);
+    player.on('playbackEnded', () => {
+      player.seek(0)
+      player.attachSource("http://0.0.0.0:8000/Dash2/dash.mpd")
+    })
+  }
 }
